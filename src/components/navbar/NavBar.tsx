da7304bda6b4d814117e37fb3bd01b6902bc9a8e
Tom Benn <tom@fridgecow.com>
2019-01-31 17:26:05 +0000

    More sections

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,7 +4,7 @@ export default class NavBar extends React.Component {
   public render() {
     return (
       <div>
-        Home | Tickets | Committee | Charity | FAQ | Employment
+        Home | Tickets | Committee | Employment | Charity | Sponsors | FAQ | T+C
       </div>
     );
   }
