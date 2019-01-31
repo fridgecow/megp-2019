4b6755d9a07ae567e3f1fa78742cf77d537349c8
Harry <farnworthharry@gmail.com>
2019-01-31 21:32:12 +0000

    Add FAQs

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import NavBar from './components/navbar/NavBar';
 import Page from './components/page/Page';
 import Pane from './components/pane/Pane';
 
+import { faq } from './lib/Faqs';
 import Logo from './logo.png';
-
 
 class App extends React.Component {
   public render() {
@@ -28,6 +28,22 @@ class App extends React.Component {
             </Page>
             <Page name="committee">
               Committee members
+            </Page>
+            <Page name="faqs">
+              {
+                faq.map(section =>
+                  <div key={section.heading}>
+                    <h1>{section.heading}</h1>
+                    {section.questions.map(qna =>
+                      <div key={qna.question}>
+                        <p><b>{qna.question}</b></p>
+                        <p>{qna.answer}</p>
+                        <p/>
+                      </div>
+                    )}
+                  </div>
+                )
+              }
             </Page>
           </Pane>
         </div>
