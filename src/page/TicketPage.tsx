a1eb145e94cfb58e12e42cc5f796ea299ccc4ac1
Harry <farnworthharry@gmail.com>
2019-02-17 13:09:07 +0000

    Automate swapping over to name change page

--- src/page/TicketPage.tsx
+++ src/page/TicketPage.tsx
@@ -18,31 +18,51 @@ export default class TicketPage extends React.Component{
   public render(){
     const time = Date.now()
 
-    if(time < 1550354400*1000){ // Before 16th Feb at 22:00
-      return (
-        <Page name="tickets" title="Tickets">
-            Tickets will be released at 12 noon on Sunday!
+    if (time < 1550491200*1000) { // Before 18th Feb at 12:00
+      return ( 
+        <Page name="tickets">
+          {
+            this.state.error ?
+              <a className="navlink" href="https://fixr.co/event/225101863">CLICK HERE TO BUY TICKETS</a>
+              :
+              <iframe src="https://fixr.co/event/225101863?compact=true"
+                    style={{
+                      width: "90vw",
+                      maxWidth: "400px",
+                      height: "550px",
+                      border: "none",
+                      marginTop: "4vh",
+                      display: this.state.loaded ? "" : "none"
+                    }} 
+                onLoad={e => this.setState({loaded: true})}
+                onError={e => this.setState({error: true}) }/>
+          }
         </Page>
       );
     }
 
     return ( 
-      <Page name="tickets">
+      <Page name="tickets" title="Name Change">
         {
-          this.state.error ?
-            <a className="navlink" href="https://fixr.co/event/225101863">CLICK HERE TO BUY TICKETS</a>
-            :
-            <iframe src="https://fixr.co/event/109609730?compact=true"
-                  style={{
-                    width: "90vw",
-                    maxWidth: "400px",
-                    height: "550px",
-                    border: "none",
-                    marginTop: "4vh",
-                    display: this.state.loaded ? "" : "none"
-                  }} 
-              onLoad={e => this.setState({loaded: true})}
-              onError={e => this.setState({error: true}) }/>
+          <div>
+            <p>Tickets to MEGP19 have now sold out. If you want to transfer any of your tickets, please purchase a transfer option below:</p>
+            {
+            this.state.error ?
+              <a className="navlink" href="https://fixr.co/event/109609730">CLICK HERE TO BUY NAME CHANGES</a>
+              :
+              <iframe src="https://fixr.co/event/109609730?compact=true"
+                    style={{
+                      width: "90vw",
+                      maxWidth: "400px",
+                      height: "550px",
+                      border: "none",
+                      marginTop: "4vh",
+                      display: this.state.loaded ? "" : "none"
+                    }} 
+                onLoad={e => this.setState({loaded: true})}
+                onError={e => this.setState({error: true}) }/>
+            }
+          </div>
         }
       </Page>
     );
